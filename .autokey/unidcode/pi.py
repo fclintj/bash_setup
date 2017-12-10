52471502dc81412d2eeea8370e26aac066f553db
Clint Ferrin <clint.ferrin@gmail.com>
2017-12-09 22:23:45 -0700

    adds unicode script from autokey

---
+++ .autokey/unidcode/pi.py
@@ -0,0 +1,13 @@
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+from subprocess import Popen, PIPE
+
+def paste_character(symbol):
+    c = Popen(['xclip', '-selection', 'clipboard'], stdin=PIPE)
+    c.communicate(symbol.encode('utf-8'))
+    keyboard.send_keys('<ctrl>+v')
+    
+paste_character('π')
+
+
